32ec7c58c31205e7810d6c5884238c4b6c6b9c18
JustYannicc <52761674+JustYannicc@users.noreply.github.com>
2023-05-10 09:22:18 +0200

    multiple pdfs per sheet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import tempfile
-
 import openpyxl
 from barcode import Code128
 from barcode.writer import ImageWriter
@@ -8,11 +7,11 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import mm
 from reportlab.pdfgen import canvas
 import math
-import qrgen
+from qrgen import codegen
 #TODO: Add to the top of the first page Serial number list with the total number of pages and barcodes
-#TODO: Make a QR code that gives a list of all devices
 #TODO: First row in spreadsheet has title and going down is all the serial numbers and the title has to be put on the first page and on the header for all subsequent pages pages
 #TODO: Every colum creates its own pdf
+#TODO: Small sealed label that says box number and sealded and do not unseal
 
 
 # Define the size of the barcode image and the spacing between them
@@ -22,76 +21,87 @@ horizontal_spacing = 0.5 * mm
 vertical_spacing = 3 * mm
 
 # Open the input spreadsheet and select the first worksheet
-workbook = openpyxl.load_workbook('input.xlsx')
+workbook = openpyxl.load_workbook('testinput.xlsx')
 worksheet = workbook.active
-            
-# Set up the PDF canvas with A4 size
-pdf_canvas = canvas.Canvas('output.pdf', pagesize=A4)
-pdf_canvas.setLineWidth(.3)
-pdf_canvas.setFont('Helvetica', 8)
 
-# Define the starting coordinates for the barcode and text
-x, y = 3.75 * mm, 280 * mm
 
-# Initialize variables to keep track of the total barcode count and the current page number
-barcode_count = 1
-current_pages = 1
-# Loop through each row and column
-count = 0
-for row in worksheet.iter_rows():
-    for cell in row:
+# Loop over all columns in the worksheet
+for column in worksheet.columns:
+    # Set up the PDF canvas with A4 size
+    name = column[0].value
+    pdf_canvas = canvas.Canvas(f'{name}.pdf', pagesize=A4)
+    pdf_canvas.setLineWidth(.3)
+    pdf_canvas.setFont('Helvetica', 8)
+
+    # Define the starting coordinates for the barcode and text
+    x, y = 3.75 * mm, 280 * mm
+
+    # Initialize variables to keep track of the total barcode count and the current page number
+    barcode_count = 1
+    current_pages = 1
+    # Loop through each row and column
+    count = 0
+    for cell in column[1:]:
         if cell.value is not None:
             count += 1
-total_pages = math.ceil(count/100)
+    total_pages = math.ceil(count/100)
+    values = []
+    
+    # Loop through each row in the column and generate a barcode for the cell data
+    for cell in column[1:]:
+        if cell.value is not None:
+            # Get the cell value
+            cell_value = cell.value
+            cell_value_parts = str(cell_value).split(".")[0] # Remove the decimal ".0" from cell data
+            values.append(cell_value_parts)
+            
+            # Generate the barcode image
+            with tempfile.NamedTemporaryFile(delete=False) as tmp:
+                options = {
+                    'module_width': 0.2,  # Make the barcode wider
+                    'module_height': 4.0,  # Make the barcode narrower
+                    'quiet_zone': 5.0,  # Leave less margin around the barcode
+                    'font_size': 0,
+                    'add_checksum': False,
+                }
+                EAN = Code128(cell_value_parts, writer=ImageWriter())
+                EAN.write(tmp, options)
+                tmp.flush()
+                barcode_image = tmp.name
+            
+            # Draw the barcode image and cell value text on the PDF canvas
+            pdf_canvas.drawImage(barcode_image, x, y, width=barcode_width, height=barcode_height)
+            text_width = pdf_canvas.stringWidth(cell_value_parts, 'Helvetica', 8)
+            pdf_canvas.drawString(x + (barcode_width - text_width) / 2, y + barcode_height + 0.5 * mm, cell_value_parts)
+            
+            # Remove the temporary barcode image file
+            os.unlink(barcode_image)
+            
+            # Move to the next barcode position
+            x += barcode_width + horizontal_spacing
+            if x > 180 * mm:
+                x = 3.75 * mm
+                y -= barcode_height + vertical_spacing
+                
+                # If we're at the bottom of the page, start a new page and add the total barcode count to the footer
+                if y < 20 * mm:
+                    codegen(values)
+                    pdf_canvas.drawString(150*mm, 10*mm, f"Page {current_pages} of {total_pages}")
+                    pdf_canvas.drawString(10*mm, 10*mm, f"Total Barcodes: {barcode_count}")
+                    barcode_count = 0
+                    current_pages += 1
+                    pdf_canvas.showPage()
+                    x, y = 3.75 * mm, 280 * mm
+                    pdf_canvas.setFont('Helvetica', 8)
+                    values.clear()
+                    #FIXME: Generates unlimted QR codes
+            
+            # Increment the barcode count for each barcode generated
+            barcode_count += 1
 
-# Loop through each row in the worksheet and generate a barcode for the cell data
-for row in worksheet.iter_rows(values_only=True):
-    # Get the cell value
-    cell_value = str(row[0]).split(".")[0]  # Remove the decimal ".0" from cell data
-    
-    # Generate the barcode image
-    with tempfile.NamedTemporaryFile(delete=False) as tmp:
-        options = {
-            'module_width': 0.2,  # Make the barcode wider
-            'module_height': 4.0,  # Make the barcode narrower
-            'quiet_zone': 5.0,  # Leave less margin around the barcode
-            'font_size': 0,
-            'add_checksum': False,
-        }
-        EAN = Code128(cell_value, writer=ImageWriter())
-        EAN.write(tmp, options)
-        tmp.flush()
-        barcode_image = tmp.name
-    
-    # Draw the barcode image and cell value text on the PDF canvas
-    pdf_canvas.drawImage(barcode_image, x, y, width=barcode_width, height=barcode_height)
-    text_width = pdf_canvas.stringWidth(cell_value, 'Helvetica', 8)
-    pdf_canvas.drawString(x + (barcode_width - text_width) / 2, y + barcode_height + 0.5 * mm, cell_value)
-    
-    # Remove the temporary barcode image file
-    os.unlink(barcode_image)
-    
-    # Move to the next barcode position
-    x += barcode_width + horizontal_spacing
-    if x > 180 * mm:
-        x = 3.75 * mm
-        y -= barcode_height + vertical_spacing
-        
-        # If we're at the bottom of the page, start a new page and add the total barcode count to the footer
-        if y < 20 * mm:
-            pdf_canvas.drawString(150*mm, 10*mm, f"Page {current_pages} of {total_pages}")
-            pdf_canvas.drawString(10*mm, 10*mm, f"Total Barcodes: {barcode_count}")
-            barcode_count = 0
-            current_pages += 1
-            pdf_canvas.showPage()
-            x, y = 3.75 * mm, 280 * mm
-            pdf_canvas.setFont('Helvetica', 8)
-    
-    # Increment the barcode count for each barcode generated
-    barcode_count += 1
-
-# Add the total number of barcodes to the last page footer
-pdf_canvas.drawString(150*mm, 10*mm, f"Page {current_pages} of {total_pages}")
-pdf_canvas.drawString(10*mm, 10*mm, f"Total Barcodes: {barcode_count}")
-pdf_canvas.save()
+    # Add the total number of barcodes to the last page footer
+    pdf_canvas.drawString(150*mm, 10*mm, f"Page {current_pages} of {total_pages}")
+    pdf_canvas.drawString(10*mm, 10*mm, f"Total Barcodes: {barcode_count}")
+    codegen(values)
+    pdf_canvas.save()
 
--- qrgen.py
+++ qrgen.py
@@ -1,29 +1,26 @@
 import qrcode
-import openpyxl
+import os
 
-# Load the Excel file
-wb = openpyxl.load_workbook('input2.xlsx')
+def codegen(values):
+    # Encode the values as a QR code
+    qr = qrcode.QRCode(version=None, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+    qr.add_data('\r'.join(values))
+    qr.make(fit=True)
 
-# Select the active worksheet
-ws = wb.active
+    # Set the file name
+    file_name = 'qr_code_1.png'
 
-# Create an empty list to store the values
-values = []
+    # Check if the file already exists
+    if os.path.isfile(file_name):
+        # Find the next available file name
+        i = 2
+        while True:
+            new_file_name = f"qr_code_{i}.png"
+            if not os.path.isfile(new_file_name):
+                break
+            i += 1
+        file_name = new_file_name
 
-# Loop through each row and append the value to the list
-for row in ws.iter_rows():
-    for cell in row:
-        if cell.value is not None:
-            # Remove the decimal ".0" from cell data
-            value = str(cell.value).split(".")[0]
-            values.append(value)
-
-# Encode the values as a QR code
-qr = qrcode.QRCode(version=None, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-qr.add_data('\r'.join(values))
-qr.make(fit=True)
-
-
-# Save the QR code image
-img = qr.make_image(fill_color='black', back_color='white')
-img.save('qr_code.png')
+    # Save the QR code image with the updated file name
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save(file_name)
